fe2687dce4267ae04730c8211883bfa193319d63
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-11-27 16:20:34 +0100

    Almost done with AccountFactory

---
+++ TH-Bank/Factories/AccountFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Principal;
+
+namespace TH_Bank
+{
+    public class AccountFactory
+    {
+        public Account CreateAccount(decimal balance, string currency, int accountNumber, string id)
+        {
+            var accountDataHandler = new AccountDataHandler();
+            var userInput = "";
+            if (userInput == "Salaryaccount")
+            {
+                Account salAccount = new SalaryAccount(balance, currency, accountNumber, id);                
+                accountDataHandler.Save(salAccount);
+                return salAccount;
+            }
+            else if (userInput == "Savingsaccount")
+            {
+                Account saveAccount = new SavingsAccount(balance, currency, accountNumber, id);
+                accountDataHandler.Save(saveAccount);
+                return saveAccount;
+            }
+            else
+            {
+                throw new Exception("Invalid account-type");
+            }
+            
+
+        }
+    }
+}
