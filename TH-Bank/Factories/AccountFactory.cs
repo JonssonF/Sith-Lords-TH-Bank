69a9c2b03eea910e8a22105a0ec1d71d60d950f6
JohanHanssonSUT24 <johanh85@hotmail.com>
2024-12-02 08:01:51 +0100

    Added "Balance" back to Accounts

--- TH-Bank/Factories/AccountFactory.cs
+++ TH-Bank/Factories/AccountFactory.cs
@@ -5,7 +5,7 @@ namespace TH_Bank
     public class AccountFactory
     {
 
-        public Account CreateAccount(string ownerid, string currency, string userchoice)
+        public Account CreateAccount(string ownerid, decimal balance, string currency, string userchoice)
         {
             int accountNumber = Format.UniqueAccountNo(userchoice);
             var accountDataHandler = new AccountDataHandler();
@@ -13,13 +13,13 @@ namespace TH_Bank
             if (userchoice == "Salaryaccount")//To.Upper
 
             {
-                Account salAccount = new SalaryAccount(ownerid, accountNumber, currency);                
+                Account salAccount = new SalaryAccount(ownerid, balance, accountNumber, currency);                
                 accountDataHandler.Save(salAccount);
                 return salAccount;
             }
             else if (userchoice == "Savingsaccount")//To.Upper
             {
-                Account saveAccount = new SavingsAccount(ownerid, accountNumber, currency);
+                Account saveAccount = new SavingsAccount(ownerid, balance, accountNumber, currency);
                 accountDataHandler.Save(saveAccount);
                 return saveAccount;
             }
