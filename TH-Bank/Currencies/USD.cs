449b63e4b8b20ca2889e1d1a14f7e788f519d565
GoodStuff15 <gustav.emil.eriksson@gmail.com>
2024-12-02 14:58:35 +0100

    Added euro and dollar classes

---
+++ TH-Bank/Currencies/USD.cs
@@ -0,0 +1,11 @@
+﻿namespace TH_Bank
+{
+    
+        public class USD : Currency
+        {
+            public override string Name { get; } = "US Dollar";
+            public override string NameShort { get; } = "USD";
+
+        }
+    
+}
