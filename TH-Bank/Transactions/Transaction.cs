100d62d3619641ad5ec1716ed78ca364d466ea9b
LinaOlandersson <olanderssonlina@gmail.com>
2024-12-04 13:43:40 +0100

    Deleted unused code in Transaction class.

--- TH-Bank/Transactions/Transaction.cs
+++ TH-Bank/Transactions/Transaction.cs
@@ -39,48 +39,6 @@ namespace TH_Bank
             
         }
 
-
-        //public bool TransferFunds() //Method for moneytransfer
-        //{
-        //    if (FromAccount.Balance >= Amount)
-        //    {
-        //        FromAccount.Balance -= Amount;
-        //        ToAccount.Balance += Amount;
-        //        return true;
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Transfer not complete. Check account balance");
-        //        return false;
-        //    }
-        //}
-
-        public void MakeTransaction(List<Account> accounts)
-        {
-            //Display all avalible accounts or option to enter new accountnumber
-            Console.WriteLine("Välj ett konto att föra över pengar ifrån: ");
-            for (int i = 0; i < accounts.Count; i++)
-            {
-                Console.WriteLine($"{i + 1}, Konto: {accounts[i].AccountNumber} Saldo: {accounts[i].Balance}");
-            }
-            int fromacc = Int32.Parse(Console.ReadLine()) - 1;
-            //Display igen?
-            Console.WriteLine("Välj ett konto att föra över pengar till: ");
-            //for (int i = 0; i < accounts.Count; i++)
-            //{
-            //    Console.WriteLine($"{i + 1}, Konto: {accounts[i].AccountNumber} Saldo: {accounts[i].Balance}");
-            //}
-            int toacc = Int32.Parse(Console.ReadLine()) - 1;
-
-            Account FromAccount = accounts[fromacc];
-            Account ToAccount = accounts[toacc];
-
-            Console.WriteLine("Ange beloppet du vill föra över: ");
-            decimal Amount = decimal.Parse(Console.ReadLine()); // IMPLEMENTERA FELHANTERING**
-
-            //var transaction = new TransactionFactory().CreateTransaction(Amount, FromAccount, ToAccount, Id);
-            //transaction.TransferFunds();
-        }
         public override string ToString()
         {
             return $"{Id}|{DateAndTime}|{Amount}|{FromAccount.AccountNumber}|{ToAccount.AccountNumber}|{Currency}|{FromAccount.OwnerID}|{ToAccount.OwnerID}";
